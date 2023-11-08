dbb35b2e83bb28d642279c7d89d5109a398ac422
Julian López <julian@MacBook-Pro-de-Julian.local>
2023-11-08 19:20:46 +0100

    fix taxonomies paths

--- taxonomies.py
+++ taxonomies.py
@@ -1,26 +1,26 @@
 import pickle as pkl
 
 # CLIP
-with open('../../utils/taxonomies/taxonomy_embeddings_clip.pkl', 'rb') as f:
+with open('./taxonomies/taxonomy_embeddings_clip.pkl', 'rb') as f:
     taxonomy_clip = pkl.load(f)
 
-with open('../../utils/taxonomies/attributes_embeddings_clip.pkl', 'rb') as f:
+with open('./taxonomies/attributes_embeddings_clip.pkl', 'rb') as f:
     attributes_clip = pkl.load(f)
 
 
 # FCLIP
-with open('../../utils/taxonomies/taxonomy_embeddings_fclip.pkl', 'rb') as f:
+with open('./taxonomies/taxonomy_embeddings_fclip.pkl', 'rb') as f:
     taxonomy_fclip = pkl.load(f)
 
-with open('../../utils/taxonomies/attributes_embeddings_fclip.pkl', 'rb') as f:
+with open('./taxonomies/attributes_embeddings_fclip.pkl', 'rb') as f:
     attributes_fclip = pkl.load(f)
 
 
-with open('../../utils/taxonomies/attributes_embeddings_clip_flat.pkl', 'rb') as f:
+with open('./taxonomies/attributes_embeddings_clip_flat.pkl', 'rb') as f:
     attributes_clip_text = pkl.load(f)
 
-with open('../../utils/taxonomies/attributes_embeddings_fclip_flat.pkl', 'rb') as f:
+with open('./taxonomies/attributes_embeddings_fclip_flat.pkl', 'rb') as f:
     attributes_fclip_text = pkl.load(f)
     
-with open('../../utils/taxonomies/db/taxonomy_embeddings_clip.pkl', 'rb') as f:
+with open('./taxonomies/db/taxonomy_embeddings_clip.pkl', 'rb') as f:
     taxonomy_db_clip = pkl.load(f)
