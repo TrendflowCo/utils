5c81cd0dcc3669b4f819854d2edeeb3e84c0f932
Julian López <julian@MacBook-Pro-de-Julian.local>
2023-11-30 10:23:15 +0100

    Changing paths in taxonomy

--- taxonomies.py
+++ taxonomies.py
@@ -9,8 +9,17 @@ with open(curr_path / Path('vocab/taxonomy_embeddings_clip.pkl'), 'rb') as f:
 
 with open(curr_path / Path('vocab/attributes_embeddings_clip.pkl'), 'rb') as f:
     attributes_clip = pkl.load(f)
+ 
+with open(curr_path / Path('vocab/db/taxonomy_embeddings_clip.pkl'), 'rb') as f:
+    taxonomy_db_clip = pkl.load(f)
 
 
+with open(curr_path / Path('vocab/db/garment_tags_fclip_text.pkl'), 'rb') as f:
+    garment_tags_fclip_text = pkl.load(f)
+    
+with open(curr_path / Path('vocab/db/cat_attrs_fclip_dict.pkl'), 'rb') as f:
+    cat_attrs_fclip_dict = pkl.load(f)
+  
 # FCLIP
 with open(curr_path / Path('vocab/taxonomy_embeddings_fclip.pkl'), 'rb') as f:
     taxonomy_fclip = pkl.load(f)
@@ -25,6 +34,6 @@ with open(curr_path / Path('vocab/attributes_embeddings_clip_flat.pkl'), 'rb') a
 with open(curr_path / Path('vocab/attributes_embeddings_fclip_flat.pkl'), 'rb') as f:
     attributes_fclip_text = pkl.load(f)
     
-with open(curr_path / Path('vocab/db/taxonomy_embeddings_clip.pkl'), 'rb') as f:
-    taxonomy_db_clip = pkl.load(f)
+with open(curr_path / Path('vocab/db/taxonomy_embeddings_fclip.pkl'), 'rb') as f:
+    taxonomy_db_fclip = pkl.load(f)
     
